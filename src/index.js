c7f9dfa7d075b115a63eff4a8de52a070b2ea357
dociledevil <khinar@trilyo.com>
2020-08-07 19:34:12 +0530

    Removed auth for testing from index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ app.post('/',(req,res)=>{
     console.log("Recieved request at ",new Date().toLocaleString('en-US',{timeZone:'Asia/Kolkata'}))
     console.log(req.body)
     console.log(JSON.stringify(req.body))
-    console.log(req.headers['epi-sig'])
-    let recieved_signature=req.headers["epi-sig"]
-    let expected_signature=crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')
-    console.log("Expected signature is ",expected_signature)
-    if (recieved_signature===expected_signature && req && req.body.namespace===namespace)
-    {
+    //console.log(req.headers['epi-sig'])
+    //let recieved_signature=req.headers["epi-sig"]
+    //let expected_signature=crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')
+    //console.log("Expected signature is ",expected_signature)
+    //if (recieved_signature===expected_signature && req && req.body.namespace===namespace)
+    //{
         try{
             let result=require.main.require('./'+req.body.function+'.js')(req.body.session,req.body.config,req.body.callback,req.body.metadata)
             res.json({"execResult":result})
@@ -24,10 +24,10 @@ app.post('/',(req,res)=>{
             res.json({"execResult":"No such method in this namespace"}).status(400)
 
         }
-    }
-    else{
-        res.status(403).end();
-    }
+    //}
+    //else{
+      //  res.status(403).end();
+    //}
     req.body;
 })
 
