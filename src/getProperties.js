bda93b6df6eb485990fc4802b4f2b74d2ba3c840
khinar-trilyo <64218099+khinar-trilyo@users.noreply.github.com>
2020-08-07 19:02:04 +0530

    Auto Commit

--- src/getProperties.js
+++ src/getProperties.js
@@ -1,19 +1,5 @@
-functions.getProperties(bot_flow.properties, session.child_botId || session.botId, function(props) {
-        let returnData = [];
-        let countriesArray = [];
-        props.map(property => {
-            let config = JSON.parse(property.config);
-            if (countriesArray.indexOf(config.country_code) == -1) {
-                countriesArray.push(config.country_code);
-                returnData.push({
-                    "content_type": "text",
-                    "title": `:flag-${config.country_code}: ${config.country}`,
-                    "payload": ["SHOW_HOSTELS", {
-                        "lead_ts_traveling_to": config.country
-                    }]
-                })
-            }
-
-        })
-        if (callback) callback(returnData);
-    });
+functions.getProperties(bot_flow.properties, session.child_botId || session.botId, function (props) {
+    let returnData = [];
+    if (callback) callback(returnData);
+    return "This is a test";
+});
