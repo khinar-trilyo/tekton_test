38850c0834cbb10dd77a0d678387ae9fa32f7ec4
khinar-trilyo <64218099+khinar-trilyo@users.noreply.github.com>
2020-08-08 16:09:21 +0530

    Auto Commit

--- src/getProperties.js
+++ src/getProperties.js
@@ -1,5 +1,5 @@
 functions.getProperties(bot_flow.properties, session.child_botId || session.botId, function(props) {
         // let returnData = [];
         // if (callback) callback(returnData);
-        return "Hello World! from dashboard. Updated."
+        return "Hello World! from dashboard. Updated. 1.0"
     });
