5a2ae5d1a14586d640c6843f4e0f1c4b1782b0b3
khinar-trilyo <64218099+khinar-trilyo@users.noreply.github.com>
2020-08-10 12:01:46 +0530

    Auto Commit

--- src/getProperties.js
+++ src/getProperties.js
@@ -1,4 +1,4 @@
 const getProperties = function(session, config, callback, metadata) {
-        return ["This is a test from the updated push - version 1"];
+        return ["This is a test from the updated push - version 2"];
 }
 module.exports=getProperties
