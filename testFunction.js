a5ffec3d2398f01292ab6ff98b77c58b5752da82
khinar-trilyo <64218099+khinar-trilyo@users.noreply.github.com>
2020-10-22 13:16:19 +0530

    Auto Commit

---
+++ testFunction.js
@@ -0,0 +1,4 @@
+functions.testFunction(bot_flow.properties, session.child_botId || session.botId, function(props) {
+        let returnData = [];
+        if (callback) callback(returnData);
+    });
